05e560ae453a0044b7ad2f15971fccae77f91e5f
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-24 17:24:57 +0200

    added main classes

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package rentpropertybid;
+package main;
 
 /**
  *
  * @author enzomourany
  */
-public class RentPropertyBid {
+public class Main {
 
     /**
      * @param args the command line arguments
