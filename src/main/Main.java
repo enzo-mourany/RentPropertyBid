2bdc98a61300e7ccbe988c7ad4738239725f5903
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-24 17:49:50 +0200

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,17 +4,20 @@
  */
 package main;
 
+
 /**
  *
  * @author enzomourany
  */
 public class Main {
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("test");
     }
     
 }
