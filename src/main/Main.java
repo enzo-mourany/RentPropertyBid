c86b905e3855779302d35df88ac1f917747655ba
Enzo Mourany <enzo.mourany@outlook.com>
2022-06-01 16:01:01 +0200

    finished login and register support

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,28 +20,34 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        Boolean connected = false;
         System.out.println("LOGIN");
-        Scanner enteredUsername = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter your username : ");
-        String userName = enteredUsername.nextLine();  // Read user input
-        if (userName.equals(Logins.USER1.username)) {
-            System.out.println("Welcome back " + userName);
-        } else {
-            System.out.println("Unknown username");
-            Scanner retry = new Scanner(System.in);  // Create a Scanner object
-            System.out.println("You have an account ? : y/n ");
-            String sRetry = retry.nextLine();
-            if (sRetry.equals("n") || sRetry.equals("no")) {
+        while (connected == false) {
+            Scanner enteredUsername = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter your username : ");
+            String userName = enteredUsername.nextLine();  // Read user input
+            if (userName.equals(Logins.USER1.username)) {
+                System.out.println("Welcome back " + userName);
+                connected = true;
+            } else {
+                System.out.println("Unknown username");
+                Scanner retry = new Scanner(System.in);  // Create a Scanner object
+                System.out.println("You have an account ? : y/n ");
+                String sRetry = retry.nextLine();
+                if (sRetry.equals("n") || sRetry.equals("no")) {
                     System.out.println("LOGIN");
-                Scanner inputNewUsername = new Scanner(System.in);  // Create a Scanner object
-                System.out.println("Enter an username : ");
-                String newUsername = inputNewUsername.nextLine();
-                Scanner inputNewPassword = new Scanner(System.in); // Create a Scanner object
-                System.out.println("Enter a password : ");
-                String newPassword = inputNewPassword.nextLine();
+                    Scanner inputNewUsername = new Scanner(System.in);  // Create a Scanner object
+                    System.out.println("Enter an username : ");
+                    String newUsername = inputNewUsername.nextLine();
+                    Scanner inputNewPassword = new Scanner(System.in); // Create a Scanner object
+                    System.out.println("Enter a password : ");
+                    String newPassword = inputNewPassword.nextLine();
+                    System.out.println("Welcome " + newUsername + " !");
+                    connected = true;
+                }
             }
         }
+
     }
     
 }
